bf183b2a7142842f710adc4d6ef9769f1bd25511
enzo <enzotobio@gmail.com>
2026-07-10 16:55:43 +0200

    setup vitest with analogjs

---
+++ src/app/example-component/example.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ExampleComponent } from './example.component';
+
+describe('ExampleComponent', () => {
+  let fixture: ComponentFixture<ExampleComponent>;
+  let component: ExampleComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ExampleComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExampleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render the component template', () => {
+    expect(fixture.nativeElement.textContent).toEqual('example-component works!');
+  });
+});
